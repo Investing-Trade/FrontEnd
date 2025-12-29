8d89f4f30f7a3f54ecc8f7827730b27dace23d6b
JeonYongHak <charismayoung1993@gmail.com>
2025-12-29 19:00:32 +0900

    로그인 화면

---
+++ Investing Trade/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // Agbalumo 폰트 패밀리 등록
+        agbalumo: ['Agbalumo', 'system-ui', 'sans-serif'],
+      },
+      colors: {
+        // 이미지의 주요 색상을 커스텀 컬러로 등록
+        'brand-blue': '#5D6DED',
+        'brand-light-blue': '#7199F1',
+        'brand-button': '#6C7EEB',
+      }
+    },
+  },
+  plugins: [],
+}
